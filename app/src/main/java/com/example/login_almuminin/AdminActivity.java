dbd5cc917478fac3d8d3548ad14ec5a05d564bac
juniantotri17 <juniantotri17@gmail.com>
2020-12-10 21:58:58 +0700

    Initial commit

---
+++ app/src/main/java/com/example/login_almuminin/AdminActivity.java
@@ -0,0 +1,16 @@
+package com.example.login_almuminin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.login_almuminin.R;
+
+public class AdminActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin);
+    }
+}
